47e09eda3058ece936e8a8df7e42bd71e54f0092
cyberglad@mail.ru <cyberglad@mail.ru>
2020-05-22 15:42:31 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SelectorComponent } from './selector/selector.component';
+import { OrganizerComponent } from './organizer/organizer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
